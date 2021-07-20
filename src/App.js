d2cfb32c59cdf274c4e1a2e6ba8f3c9243b51705
JacxDev <javierc@xinnux.com>
2021-07-20 02:04:17 -0500

    add custom hook useFetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
+import Item from './components/Item';
+import useFetch from './hooks/useFetch';
 
 function App() {
+
+  const token = process.env.REACT_APP_APIKEY;
+  // const uri = `https://superheroapi.com/api/${token}/1`
+  const uri = `https://superheroapi.com/api/1002618803896919/1`
+
+  const { loading, data, error } = useFetch(uri);
+
+  if(!loading)
+  if(!error)
+
+  console.log(data)
+
   return (
     <div className="container">
         <div className="element-container">
             <div className="grid-items">
-                <div className="item">
-                    element1
-                </div>
-                <div className="item">
-                    element2
-                </div>
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
+                <Item />
             </div>
             <div className="avatar-container">
                 element2
---
+++ src/components/Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './item.css'
+
+const Item = () => {
+    return (
+        <div className="item"> 
+            Element 1
+        </div>
+    )
+}
+
+export default Item
