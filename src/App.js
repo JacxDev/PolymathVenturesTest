b13ccadd6ba5175a0ccc1587b96caea5d4151357
JacxDev <javierc@xinnux.com>
2021-07-20 01:34:14 -0500

    avance grid responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="element-container">
+            <div className="grid-items">
+                <div className="item">
+                    element1
+                </div>
+                <div className="item">
+                    element2
+                </div>
+            </div>
+            <div className="avatar-container">
+                element2
+            </div>
+        </div>
     </div>
   );
 }
