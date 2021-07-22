51400cf241aa4b197bd838aaaa079d4cca5f0ae2
JacxDev <javierc@xinnux.com>
2021-07-22 16:10:18 -0500

    character info add

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -5,22 +5,32 @@ import './character.css';
 
 const Character = () => {
     const location = useLocation()
-    const { character:{image, name} } = location.state
+    const { character:{image, name, powerstats:{ combat, intelligence, power, speed } ,appearance:{gender, race}} } = location.state
     
+    console.log(location.state)
+
 
     return (
         <div className="container-characterInfo">
-            
-            <Link to="/">
-                Back
-            </Link>
-            
             <div className="character-info">
                 <div className="character-text">
                     <h2>INFO</h2>
                     <h4> { name } </h4>
+                    <h4> { `Gender:  ${gender}`} </h4>
+                    <h4> { `Race: ${ race }` } </h4>
+                    <h2>Stats</h2>
+                    <h4>  { `Combat:  ${ combat }` }  </h4>
+                    <h4> { `intelligence: ${ intelligence }` } </h4>
+                    <h4> { `Power: ${power}` } </h4>
+                    <h4> { `Speed ${ speed }` } </h4>
+
+                    <button>
+                        <Link to="/">
+                            Go Back
+                        </Link>
+                    </button>
                 </div>
-                <div>
+                <div className="character-img"> 
                     <img src={ image.url } alt="" />
                 </div>
             </div>
