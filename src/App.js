e76994bed28bc220cdb9da0de3b3f64819a71773
JacxDev <javierc@xinnux.com>
2021-07-21 22:07:28 -0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
 import React, { useState } from 'react'
 import './App.css';
-import Avatar from './components/Avatar';
-import useFetch from './hooks/useFetch';
+import Main from './pages/Main';
+import Character from './pages/Character';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useParams
+  } from "react-router-dom";
 
 function App() {
-
-  const [ urlAvatar, setUrlAvatar ] = useState("");
-  const [ name, setName ] = useState("")
-
-  const token = process.env.REACT_APP_APIKEY;
-  const uri = `https://superheroapi.com/api/${token}`
-
-  const { loading, error, data } = useFetch(uri, 32);
-
-  const handleHover = (e) => {
-    setUrlAvatar(e.target.src);
-    setName(e.target.alt);
-  }
-
-  if(loading) return <p>Loading...</p>
-
   return (
-    <div className="container">
-        <div className="element-container">
-            <div className="grid-items">
-              {
-                data.map((e) => {
-                  return(
-                    <div className="item"  >  
-                      <img alt={ e.name }  onMouseEnter={handleHover} src={ e.image.url } /> 
-                    </div>
-                  )
-                })
-              }
-            </div>
-            <div className="avatar-container">
-                <Avatar img={ urlAvatar } name={ name } />
-            </div> 
-        </div>
-    </div>
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Main />
+				</Route>
+				<Route path="/character">
+					<Character />
+				</Route>
+			</Switch>
+		</Router>
   );
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Avatar from '../components/Avatar';
+import useFetch from '../hooks/useFetch';
+
+const Main = () => {
+    const [ urlAvatar, setUrlAvatar ] = useState("");
+  const [ name, setName ] = useState("")
+
+  const token = process.env.REACT_APP_APIKEY;
+  const uri = `https://superheroapi.com/api/${token}`
+
+  const { loading, error, data } = useFetch(uri, 32);
+
+  const handleHover = (e) => {
+    setUrlAvatar(e.target.src);
+    setName(e.target.alt);
+  }
+
+  if(loading) return <p>Loading...</p>
+
+  return (
+    <div className="container">
+        <div className="element-container">
+            <div className="grid-items">
+              {
+                data.map((e) => {
+                  return(
+                    <div className="item"  >  
+                      <img alt={ e.name }  onMouseEnter={handleHover} src={ e.image.url } /> 
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className="avatar-container">
+                <Avatar img={ urlAvatar } name={ name } />
+            </div> 
+        </div>
+    </div>
+  );
+}
+
+export default Main
